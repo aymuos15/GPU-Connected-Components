9216fe8a7b3b153c1a3fcff97606817b11cfbc2f
Soumya Snigdha Kundu <soumya@sie209-lap.isd.kcl.ac.uk>
2024-07-05 07:49:52 +0100

    first

---
+++ cc-3d.py
@@ -0,0 +1,12 @@
+import torch
+
+import cupy as cp
+from cucim.skimage import measure as cucim_measure
+
+def get_connected_components(img, connectivity=None):
+
+    img_cupy = cp.asarray(img)
+    labeled_img, num_features = cucim_measure.label(img_cupy, connectivity=connectivity, return_num=True)
+    labeled_img_torch = torch.as_tensor(labeled_img, device=img.device)
+
+    return labeled_img_torch, num_features
