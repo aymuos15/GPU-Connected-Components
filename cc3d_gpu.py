2105a6b82fbfb06b2e2329655fc8da43739493b0
Soumya Snigdha Kundu <soumya_snigdha.kundu@kcl.ac.uk>
2025-03-19 20:28:50 +0000

    repo restructure

--- cc3d_gpu.py
+++ cc3d_gpu.py
@@ -3,7 +3,7 @@ import torch
 import cupy as cp
 from cucim.skimage import measure as cucim_measure
 
-def get_connected_components(img, connectivity=None):
+def gpu_connected_components(img, connectivity=None):
 
     img_cupy = cp.asarray(img)
     labeled_img, num_features = cucim_measure.label(img_cupy, connectivity=connectivity, return_num=True)
